ca7d51ad1870fff294fb9f75fdd6e469e64b0ece
Rifat Mahmud <rifatmahmudpc@gmail.com>
2026-03-26 11:16:03 +0600

    no fixing

--- check_number.m
+++ check_number.m
@@ -3,7 +3,7 @@ clc, clearvars;
 num = input("Input number to check: ");
 
 if is_prime(num)
-    disp(num+" is a prime number");
+    disp(num + " is a prime number");
 else
-    disp(num+" is not a prime number");
+    disp(num + " is not a prime number");
 end
